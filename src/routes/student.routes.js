01c2c50b399cd8b38e3872abe500d7422748f9c8
Nana-Koramah <n.abeasi@alustudent.com>
2025-07-30 21:58:48 +0200

    student routes update

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/student.controller');
-const { authenticate, authorizeManager } = require('../middlewares/auth.middleware');
+const { authenticate, authorizeRoles } = require('../middlewares/auth.middleware');
 
-router.get('/', authenticate, studentController.getStudents);
-router.get('/:id', authenticate, studentController.getStudentById);
-router.post('/', authenticate, authorizeManager, studentController.createStudent);
-router.put('/:id', authenticate, authorizeManager, studentController.updateStudent);
-router.delete('/:id', authenticate, authorizeManager, studentController.deleteStudent);
+// GET routes – accessible by both managers and facilitators
+router.get('/', authenticate, authorizeRoles(['manager', 'facilitator']), studentController.getStudents);
+router.get('/:id', authenticate, authorizeRoles(['manager', 'facilitator']), studentController.getStudentById);
+
+// POST, PUT, DELETE – only accessible by managers
+router.post('/', authenticate, authorizeRoles(['manager']), studentController.createStudent);
+router.put('/:id', authenticate, authorizeRoles(['manager']), studentController.updateStudent);
+router.delete('/:id', authenticate, authorizeRoles(['manager']), studentController.deleteStudent);
 
 module.exports = router;
+
